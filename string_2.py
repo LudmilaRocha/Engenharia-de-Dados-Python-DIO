df3d19cb23d8b75ef154621e761c5056c990b86d
Ludmila <ludmila030301@gmail.com>
2024-09-24 20:54:56 -0300

    Update string_2.py
    Update de metodos .format para expressões de string.

--- string_2.py
+++ string_2.py
@@ -2,5 +2,16 @@ nome = "Ludmila"
 idade = 23
 profissao = "programador"
 linguagem = "python"
+saldo= 45.55
+dados = {"nome": "Ludmila", "idade": 23}
+
 
 print("Nome: %s Idade: %d" % (nome, idade))
+print("Nome: {} Idade: {}".format(idade, nome))
+print("Nome: {1} Idade: {0}".format(idade, nome))
+print("Nome: {1} Idade: {0} Nome: {1} {1}".format(idade, nome))
+print("Nome: {nome} Idade: {idade}".format(nome = nome, idade = idade))
+print("Nome: {name} Idade: {age} {name} {name} {age}".format(age = idade, name= nome))
+print("Nome: {nome} Idade: {idade}".format(**dados))#contatenando
+print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:10.2f}")
+    
