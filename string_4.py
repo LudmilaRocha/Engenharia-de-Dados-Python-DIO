4fa7cb88a9221f13a94193e57ac42c030c43fea7
Ludmila <ludmila030301@gmail.com>
2024-09-25 19:39:49 -0300

    Update
    Update da aula de string de recuo para aplicações de Menu.

---
+++ string_4.py
@@ -0,0 +1,21 @@
+nome = "Ludmila"
+
+mensagem = f'''
+Olá meu nome é {nome}, 
+Eu estou aprendendo Python.
+    Essa mensagem tem diferentes recuos.nome
+
+'''
+print(mensagem)
+
+#teste aula exercicio de recuo em uma mensagem de menu 
+
+print('''
+      ----------------Menu-------------
+
+      1 - Depositar
+      2 - Sacar
+      0 - Sair
+      ---------------------------------
+                Obrigada por usar o nosso sistema!!
+     ''' )
