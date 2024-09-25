9e497ee4b00d82a140bfa5328341691cd46959b0
Ludmila <ludmila030301@gmail.com>
2024-09-25 19:34:04 -0300

    Update aula dio
    Update de strings utilizando algumas opções de fatimento.

---
+++ string_3.py
@@ -0,0 +1,11 @@
+nome = "Ludmila Rocha Silva"
+
+#fatiamento de string
+print(nome[0])
+print(nome[:9])
+print(nome[10:])
+print(nome[10:16])
+print(nome[10:16:2])
+print(nome[:])
+#trabalha de traz para frente, sendo negativo
+print(nome[::-1])
