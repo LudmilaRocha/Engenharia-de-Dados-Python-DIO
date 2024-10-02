b72920f1a2a462c424cad41537492575907f7352
Ludmila <ludmila030301@gmail.com>
2024-10-02 19:25:08 -0300

    Update 06_funcoes_salario.py
    Atualização

--- 06_funcoes_salario.py
+++ 06_funcoes_salario.py
@@ -2,8 +2,16 @@ salario = 2000
 
 def salario_bonus(bonus):
     global salario
+    lista_aux = lista.copy()
+    lista_aux.append(2)
+    print(f"lista aux = {lista_aux}")
+
+
     salario += bonus
     return salario
+#iten imutavel
+lista = [1]
 
 novo_salario = salario_bonus(500) #2500
 print(novo_salario)
+print(lista)
