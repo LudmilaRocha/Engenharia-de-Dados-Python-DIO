fb0204f7cc511c297a5e99fc182b977132e3f36b
Ludmila <ludmila030301@gmail.com>
2024-10-23 20:09:08 -0300

    update
    update listas em python

---
+++ Estrutura_de_dados/Listas/00_declarando_listas.py
@@ -0,0 +1,28 @@
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list("python")
+print(letras)
+
+
+frutas = ["maçã", "laranja","uva"]
+print(frutas[0])
+print(frutas[2]) #se for negativo ele conta de traz para frente
+
+#matriz
+
+matriz = [
+    [1 , 2, 3]
+    [4, 5, 6]
+    [7, 8 , 9]
+]
+print(matriz[1][2])
+
+#percorrendo a lista
+carros = ["gol", "celta", "palio"]
+
+for carro in carros:
+    print(carros)
